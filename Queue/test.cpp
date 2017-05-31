ff824a1ed16d34b20a4ff80f5e8b0c8e9435bee7
Reanimator <Ageev.mikhail.23@gmail.com>
2017-05-31 13:05:26 +0300

    queue

---
+++ Queue/test.cpp
@@ -0,0 +1,31 @@
+#include "Queue.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	try
+	{
+
+
+		Queue <int> temp(200);
+
+		temp.push(1235);
+		temp.push(437);
+		temp.push(2225);
+		temp.push(4110);
+		temp.resize(9);
+		cout << temp.pop(); 
+		cout << " " << temp.pop(); 
+		cout << " " << temp.pop() << endl;
+	}
+	catch (int & e)
+	{
+		cout << "error number: " << e << endl;
+	}
+	cin.get();
+	return 0;
+}
+
+
